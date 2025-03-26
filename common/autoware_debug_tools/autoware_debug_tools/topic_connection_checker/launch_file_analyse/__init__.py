9295a0ada8b83c7181ee9963f5e873500cf9267b
Ryohei Sasaki <ryohei.sasaki@example.com>
2025-03-26 14:35:04 +0900

    Delete except for localization

--- common/autoware_debug_tools/autoware_debug_tools/topic_connection_checker/launch_file_analyse/__init__.py
+++
@@ -1,5 +0,0 @@
-from .launch_tree import LaunchTree  # noqa
-from .launch_tree import LaunchTreeNode  # noqa
-from .main import launch_file_analyse_main  # noqa
-from .string_utils import find_cmake_projects  # noqa
-from .string_utils import find_package  # noqa
--- map/autoware_lanelet2_map_utils/README.md
+++
@@ -1,3 +0,0 @@
-# autoware_lanelet2_map_utils
-
-This package is for preprocessing the lanelet map.
